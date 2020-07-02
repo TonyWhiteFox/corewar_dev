8b95b2253cb4900259cea07944d0ce48ba67489d
mmanhack <mmanhack@school21>
2020-07-02 23:41:00 +0300

    added dump output (-a) flag - working, but not finished

--- src/output.c
+++ src/output.c
@@ -18,7 +18,47 @@ static void		write_code(t_serv *s)
 
 }
 
-void	output(t_serv *s)
+static void		print_arg(t_instr *ptr, int i)
+{
+	if (ptr->args[i].type == T_DIR)
+		write(1, "%", 1);
+	else if (ptr->args[i].type == T_REG)
+		write(1, "r", 1);
+	if (ptr->args[i].label)
+		ft_printf(":%-16s", ptr->args[i].label);
+	else
+		ft_printf("%-17d", ptr->args[i].value);
+}
+
+static void		print_code(t_serv *s)
+{
+	t_instr		*ptr;
+	size_t		i;
+	int			j;
+
+	ft_printf("Dumping annotated program on standard output\n");
+	ft_printf("Program size : %d bytes\n", s->header.prog_size);
+	ft_printf("Name : \"%s\"\n", s->header.prog_name);
+	ft_printf("Comment : \"%s\"\n", s->header.comment);
+	i = 0;
+	ptr = s->instr;
+	while (ptr)
+	{
+		if (ptr->label)
+			ft_printf("%-10d:    %s:\n", i, ptr->label);
+		ft_printf("%-5d(%-2d) :        ", i, ptr->size);
+		ft_printf("%-10s", ptr->op->name);
+		j = -1;
+		while (++j < 3 && (ptr->args[j].value != INT32_MAX
+		|| ptr->args[j].label))
+			print_arg(ptr, j);
+		ft_printf("\n");
+		i += ptr->size;
+		ptr = ptr->next;
+	}
+}
+
+void			output(t_serv *s)
 {
 	char	*file;
 	char	*tmp;
