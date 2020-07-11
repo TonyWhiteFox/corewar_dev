61c8de9268ac618d530ccc13aca4a15161b1ce8d
TonyWhiteFox <Tlpuzpak@gmail.com>
2020-07-11 14:52:51 +0300

    final

---
+++ champ/gnat.s
@@ -0,0 +1,17 @@
+#deassembler par msa 1.8, made by joe
+.name "gnat"
+.comment "bz-bz"
+
+	 ld    %0,r3
+	 live  %r0
+     live  %r0
+     live  %r0
+     live  %r0
+     live  %r0
+     live  %r0
+     live  %r0
+     live  %r0
+     live  %r0
+     st    r1,6
+     live  %r0
+	 zjmp  %-5
