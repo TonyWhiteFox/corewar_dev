d196e79938e70c8f5bafa8a0adce9a9d4da7f546
TonyWhiteFox <Tlpuzpak@gmail.com>
2020-07-11 11:40:06 +0300

    Final with out norm

---
+++ obj/corewar/hide_show_run_vm 2.d
@@ -0,0 +1,13 @@
+obj/corewar/hide_show_run_vm.o: src/corewar/hide_show_run_vm.c \
+  includes/corewar/corewar.h libft/includes/libft.h \
+  libft/includes/ft_printf.h miniLibX/mlx.h includes/corewar/op.h
+
+includes/corewar/corewar.h:
+
+libft/includes/libft.h:
+
+libft/includes/ft_printf.h:
+
+miniLibX/mlx.h:
+
+includes/corewar/op.h:
---
+++ obj/dasm/header 2.d
@@ -0,0 +1,10 @@
+obj/dasm/header.o: src/dasm/header.c includes/dasm/dasm.h \
+  libft/includes/libft.h libft/includes/ft_printf.h includes/dasm/op.h
+
+includes/dasm/dasm.h:
+
+libft/includes/libft.h:
+
+libft/includes/ft_printf.h:
+
+includes/dasm/op.h:
